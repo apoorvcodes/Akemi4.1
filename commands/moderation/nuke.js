cb7643c3e300a6d29902f223419a1b4d14385b43
Apoorv Singh <rekhasingh15062010@gmail.com>
2021-01-27 18:40:27 +0530

    Update nuke.js

--- commands/moderation/nuke.js
+++ commands/moderation/nuke.js
@@ -3,21 +3,24 @@ const Discord = require('discord.js');
 module.exports = {
     name: "nuke",
     description: "",
+const Discord = require('discord.js');
+
+module.exports = {
+    name: "nuke",
+    description: "",
     usage: "",
     enabled: true,
     aliases: [''],
-    memberPermissions: [""],
-    botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],
+    memberPermissions: ["MANAGE_CHANNELS"],
+    botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS","MANAGE_CHANNELS" ],
     nsfw: false,
     cooldown: 3000,
     ownerOnly: false,
  
      async execute(client, message, args, data) {
-        if(!message.guild.me.hasPermission("MANAGE_CHANNELS")) return message.channel.send('I don\'t have the right permissions.')
-        if (!message.member.hasPermission("MANAGE_CHANNELS")) {
-            return message.channel.send("You Don't Have Permission!")
+      
            
-        }
+        
         message.channel.clone().then
         ((ch) => {
             ch.setParent(message.channel.parent);
