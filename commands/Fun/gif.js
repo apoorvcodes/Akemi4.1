480f5bdf6d4e3d46891e41f0741b13785b5550ee
Apoorv Singh <rekhasingh15062010@gmail.com>
2021-01-28 19:03:30 +0530

    Update gif.js

--- commands/Fun/gif.js
+++ commands/Fun/gif.js
@@ -7,7 +7,7 @@ module.exports = {
   usage: "",
   enabled: true,
   aliases: [''],
-  memberPermissions: [""],
+  memberPermissions: [],
   botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],
   nsfw: false,
   cooldown: 3000,
