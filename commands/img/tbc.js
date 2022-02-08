6533384a0040e91a88fa152af560076d902beefd
ToTu-Dev <75479355+ToTu-Dev@users.noreply.github.com>
2022-02-08 20:12:12 +0530

    feat:img commands

---
+++ commands/img/tbc.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+ const amclient = require("../../helpers/amapi")
+module.exports = {
+  name: "tobecontinued",
+  description: "",
+  usage: "",
+  enabled: true,
+  aliases: ['tbc'],
+  memberPermissions: [],
+  botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],
+  nsfw: false,
+  cooldown: 3000,
+  ownerOnly: false,
+   
+     async execute(client, message, args, data) {
+	let User = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase().includes() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase().includes() === args.join(' ').toLocaleLowerCase()) || message.member;
+	let args1 = args.slice(1).join(" ")
+        let m = await message.channel.send("**Please Wait...**");
+        let buffer = await amclient.generate("tobecontinued", { url: User.user.displayAvatarURL({ format: "png", size: 2048 }), text: args1 });
+       
+        let attachment = new Discord.MessageAttachment(buffer, "mission.png");
+        m.delete({ timeout: 5000 });
+        message.channel.send(attachment);
+        
+    }}
---
+++ helpers/amapi.js
@@ -0,0 +1,6 @@
+
+const AmeClient = require('amethyste-api');
+
+const  AME_API = ('bb03f373caa534fcfcbaeae177a65134f44a6e57ba7a7b098be273867b376d8a677ddae3c23c6ded4fec8288573945e8c3483689deb13f229376ad4b5b60231d')
+const AmeAPI = new AmeClient(AME_API);
+module.exports = AmeAPI
